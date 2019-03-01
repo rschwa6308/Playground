f8e249cbcfbbe209c27e29ff0712d4da3438a2c4
Russell Schwartz <russell.w.schwartz@gmail.com>
2019-02-28 23:10:30 -0500

    added clack sound

--- Balls.py
+++ Balls.py
@@ -9,6 +9,8 @@ from itertools import combinations
 
 class Body:
     fixed = False
+    collision_count = 0
+    collision_sound = None
 
     def __init__(self, pos, vel, mass, color=None, elasticity=None):
         self.pos = V2(pos)
@@ -42,7 +44,7 @@ class Body:
         if isinstance(self, Ball) and isinstance(other, Ball):
             if self.pos.distance_to(other.pos) < self.radius + other.radius:
                 d = self.pos.distance_to(other.pos)
-                n = (other.pos - self.pos) / d
+                n = (other.pos - self.pos) / max(d, 0.000000001)
                 p = 2 * (self.vel.dot(n) - other.vel.dot(n)) / (self.mass + other.mass)
                 self.vel -= p * other.mass * n * self.elasticity  # note: not sure if this actually conserves momentum (?)
                 other.vel += p * self.mass * n * other.elasticity
@@ -51,6 +53,7 @@ class Body:
                 desired_distance = self.radius + other.radius
                 self.pos -= n * (desired_distance - d) * other.mass / (self.mass + other.mass)
                 other.pos += n * (desired_distance - d) * self.mass / (self.mass + other.mass)
+                return True
 
         else:
             ball = self if isinstance(self, Ball) else other
@@ -61,7 +64,9 @@ class Body:
                 if platform.pos.y - ball.radius < ball.pos.y < platform.pos.y + platform.height + ball.radius:
                     ball.pos.y = platform.pos.y - ball.radius if ball.vel.y > 0 else platform.pos.y + platform.height + ball.radius
                     ball.vel.y *= -ball.elasticity
+                    return True
 
+        return False
 
 
 class Ball(Body):
@@ -85,16 +90,22 @@ class Ball(Body):
         if self.pos.x < self.radius:
             self.vel.x *= -self.elasticity
             self.pos.x = self.radius
+            return True
         elif self.pos.x > room_width - self.radius:
             self.vel.x *= -self.elasticity
             self.pos.x = room_width - self.radius
+            return True
 
         if self.pos.y < self.radius:
             self.vel.y *= -self.elasticity
             self.pos.y = self.radius
+            return True
         elif self.pos.y > room_height - self.radius:
             self.vel.y *= -self.elasticity
             self.pos.y = room_height - self.radius
+            return True
+
+        return False
 
 
 class Platform(Body):
@@ -127,10 +138,17 @@ class Simulation:
         for body in self.bodies:
             if not body.fixed:
                 body.apply_gravity(self.g, time_step)
-                body.collide_walls(self.room_width, self.room_height)
+                if body.collide_walls(self.room_width, self.room_height):
+                    Body.collision_count += 1
+                    if body.collision_sound is not None:
+                        body.collision_sound.play()
 
         for body_pair in combinations(self.bodies, 2):
-            body_pair[0].collide_body(body_pair[1], time_step)
+            if body_pair[0].collide_body(body_pair[1], time_step):
+                Body.collision_count += 1
+                print(Body.collision_count)
+                if body_pair[0].collision_sound is not None:
+                    body_pair[0].collision_sound.play()
 
         for body in self.bodies:
             if not body.fixed:
@@ -154,13 +172,14 @@ def get_simulation():
     #     bodies.append(Ball(pos, vel, mass, radius, elasticity=e))
     # bodies.append(Platform((1, 1), (2, 0.5)))
 
-    # bodies.append(Ball((4, 2), (0, 0), 50, 0.50))
-    # bodies.append(Ball((4, 4), (0, 0), 1, 0.10))
-    # bodies.append(Ball((4, 4.75), (0, 0), 50, 0.50))
+    r = 50
+    bodies.append(Ball((4, 2), (0, 0), r, 0.50))
+    bodies.append(Ball((4, 4), (0, 0), 1, 0.10))
+    bodies.append(Ball((4, 4.75), (0, 0), r, 0.50))
 
-    n = 10
-    for i in range(n):
-        bodies.append(Ball((4, i*room_dimensions[1]/n), (0, 0), 50, room_dimensions[1]/(4*n)))
+    # n = 50
+    # for i in range(n):
+    #     bodies.append(Ball((4, i*room_dimensions[1]/n), (0, 0), 50, room_dimensions[1]/(4*n)))
 
     return Simulation(room_dimensions, bodies)
 
@@ -170,9 +189,15 @@ def main():
     sim = get_simulation()
     sim_time_scale = 1.0  # adjusts speed of simulation without effecting frame rate
 
-    # Initialize pygame and the display
+    # Initialize pygame elements and the display
+    pygame.mixer.init(22100, -16, 2, 32)        # must be called before pg.init()
+    clack_sound = pg.mixer.Sound("clack.wav")
+    Ball.collision_sound = clack_sound
+
     screen = get_screen((800, 600))
+
     fps_font = pg.font.SysFont("Segoe UI", 20)
+
 
     # Initialize timing system (hard max of 1000fps)
     ui_frame_rate = 60  # controls ui frame rate (should be left at 60)
